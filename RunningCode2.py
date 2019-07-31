6fabac53bb84d2a1df874565095e795646136157
Saanjh Khattar <49347021+saanjhkhattar2@users.noreply.github.com>
2019-07-31 14:56:41 -0400

    Add files via upload

--- RunningCode2.py
+++ RunningCode2.py
@@ -46,12 +46,34 @@ Cadbury3 = "White Chocolate"
 
 print(Cadbury1, Cadbury2, Cadbury3)
 
+Cadburymilk = 6
+Cadburydark = 5
+Cadburywhite = 8
+
+print(Cadburymilk, Cadburydark, Cadburywhite)
+
+chocolate1 = {Cadbury1: 6}
+print(chocolate1)
+chocolate2 = {Cadbury2: 5}
+print(chocolate2)
+chocolate3 = {Cadbury3: 8}
+print(chocolate3)
+
+#7/31/19
+#using the dict function
+Nameage = {"Steve":(32,"Male"), "Lia":(28, "Female"), "Vin":(45,"Male"), "Katie":(38,"Female")}
+print(Nameage)
+
+#using lists
+studentlist = [["steve", 32, "M"], ["lia", 28, "F"], ["vin", 45, "M"], ["katie", 38, "F"]]
+print(studentlist)
 #representing types of chocolate in the box
 def cadburyBox(m,w,d):
     print("There are", m, ",", w, ",", d, "in the box")
     
 cadburyBox("Milk chocolate", "White chocolate", "Dark chocolate")
 
+#new lesson
 print('Enter your name:')
 x = input()
 print('Hello, ' + x)
@@ -79,3 +101,40 @@ cuberoot(27)
 print('Enter a value:')
 x = int(input())
 print('The cubic root of', x, "is", math.pow(x,(1/3)))
+
+#PANDAS USAGE
+import pandas
+dir(pandas)
+
+studentdf = pandas.DataFrame(studentlist, columns=("name", "age", "gender"))
+print(studentdf)
+
+chocolates = [["milk", 5], ["dark", 8], ["white", 3]]
+chocodf = pandas.DataFrame(chocolates, columns = ("chocolate", "quantity"))
+print(chocodf)
+ 
+#afternoon 
+studentdf["name"]
+studentdf["age"]
+
+studentdf2 = pandas.DataFrame(studentlist,columns=("name", "age", "gender"), index=["1","2","3","4"])
+studentdf2
+
+
+#plotting the data
+import plotly
+dir(plotly)
+
+from plotly.offline import plot 
+import plotly.graph_objs as go
+
+studentbar = go.Bar(x=studentdf["name"], y=studentdf["age"])
+plot([studentbar])
+
+chocobar = go.Bar(x=chocodf["chocolate"], y=chocodf["quantity"])
+plot([chocobar])
+
+titles = go.Layout(title = "number of chocolates by type")
+chocobar = go.Bar(x=chocodf["chocolate"], y=chocodf["quantity"])
+fig = go.Figure(data=[chocobar], layout=titles)
+plot(fig)
